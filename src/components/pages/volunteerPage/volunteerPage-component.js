c81f1ad5b20bb72e2543c10d853db564b34661ec
Andrew Truong <andrew.b.truong@accenture.com>
2020-11-26 09:43:26 +1100

    added maps and environment var to hide api keys

--- src/components/pages/volunteerPage/volunteerPage-component.js
+++ src/components/pages/volunteerPage/volunteerPage-component.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps"
+require('dotenv').config()
 
-const VolunteerPage = () => { 
 
-    return( 
-        <div> Volunteer Here! </div>
+function Map() {
+    return(
+        <GoogleMap 
+            defaultZoom={10} 
+            defaultCenter={{lat:45.421532, lng: -75.697189 }} 
+        /> 
     )
 }
 
-export default VolunteerPage; 
+const WrappedMap = withScriptjs(withGoogleMap(Map));
+
+const VolunteerPage = () => { 
+
+    return(
+        
+        <div style={{width: '100vw', height: '100vh'}}>
+
+            <WrappedMap 
+                googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_KEY}&v=3.exp&libraries=geometry,drawing,places`}
+                loadingElement={<div style={{height:"100%" }}/>}
+                containerElement = {<div style={{ height:"400px"}}/> }
+                mapElement = {<div style={{height: "100%"}}/>}
+            />
+
+        </div>
+    )
+}
+console.log(process.env)
+export default VolunteerPage;   
